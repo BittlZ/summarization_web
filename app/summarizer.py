eaa2a74018237d9f1b7feaf3c7eaaee98db89e1e
Ivan Pokrovsky <ivan.d.pokrovsky@gmail.com>
2024-06-09 23:21:04 +0300

    update

--- app/summarizer.py
+++
@@ -1,7 +0,0 @@
-from sumy_algorithm import summarize_text
-from tokenizer import tokenize_text  # Пример дополнительного импорта
-
-def summarize(text):
-    tokens = tokenize_text(text)  # Использование метода из tokenizer.py
-    summary = summarize_text(tokens)  # Использование метода из sumy_algorithm.py
-    return summary
